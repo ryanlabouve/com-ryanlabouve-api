da5b11ea93315a4b9ebb6e5485421f7e4c5d1ccb
Ryan LaBouve <imryanlabouve@gmail.com>
2016-12-25 01:53:31 -0600

    Add search filter

--- app/resources/api/v1/article_resource.rb
+++ app/resources/api/v1/article_resource.rb
@@ -8,6 +8,14 @@ class Api::V1::ArticleResource < JSONAPI::Resource
 
   filter :slug
 
+  filter :limit, apply: ->(records, value, _options) {
+    records.limit(value[0])
+  }
+
+  filter :term, apply: ->(records, value, _options) {
+    records.where("content LIKE ?", "%#{value[0]}%")
+  }
+
   def self.default_sort
     [{field: 'date', direction: :desc}]
   end
